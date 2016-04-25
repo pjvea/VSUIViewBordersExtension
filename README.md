03b28cbc735eb804e60496d9af9550092796c441
Vea Software <pj@veasoftware.com>
2016-04-25 11:28:38 -0600

    Init Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+VSUIViewBordersExtension
+============
+
+
+VSUIViewBordersExtension is an extension of UIView that allows you to edit the cornerRadius, borderWidth, borderColor, leftBorder, topBorder, rightBorder and bottomBorder in Interface Builder.
+
+
+Installation
+============
+
+
+Add VSUIViewBordersExtension.swift to your project.
+
+
+Usage
+=====
+
+After adding VSUIViewBordersExtension.swift to your project you can edit these attributes in Interface Builder.
+
+![Interface Builder](/Interface Builder.png?raw=true "Interface Builder")
---
+++ VSUIViewBordersExtension.swift
@@ -0,0 +1,107 @@
+//
+//  VSUIViewBordersExtension.swift
+//
+//  Created by PJ Vea on 4/24/16.
+//  Copyright © 2016 Vea Software. All rights reserved.
+//
+
+import UIKit
+
+extension UIView
+{
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        set {
+            self.layer.cornerRadius = newValue
+            self.layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(CGColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue?.CGColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return self.layer.borderWidth
+        }
+        set {
+            self.layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var leftBorder: CGFloat {
+        get {
+            return 0.0
+        }
+        set {
+            let line = UIView(frame: CGRect(x: 0.0, y: 0.0, width: newValue, height: self.bounds.height))
+            line.translatesAutoresizingMaskIntoConstraints = false
+            line.backgroundColor = self.borderColor
+            self.addSubview(line)
+            
+            let views = ["line": line]
+            let metrics = ["lineWidth": newValue]
+            self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|[line(==lineWidth)]", options: [], metrics: metrics, views: views))
+            self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[line]|", options: [], metrics: nil, views: views))
+        }
+    }
+    
+    @IBInspectable var topBorder: CGFloat {
+        get {
+            return 0.0
+        }
+        set {
+            let line = UIView(frame: CGRect(x: 0.0, y: 0.0, width: self.bounds.width, height: newValue))
+            line.translatesAutoresizingMaskIntoConstraints = false
+            line.backgroundColor = self.borderColor
+            self.addSubview(line)
+            
+            let views = ["line": line]
+            let metrics = ["lineWidth": newValue]
+            self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|[line]|", options: [], metrics: nil, views: views))
+            self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[line(==lineWidth)]", options: [], metrics: metrics, views: views))
+        }
+    }
+    
+    @IBInspectable var rightBorder: CGFloat {
+        get {
+            return 0.0
+        }
+        set {
+            let line = UIView(frame: CGRect(x: self.bounds.width, y: 0.0, width: newValue, height: self.bounds.height))
+            line.translatesAutoresizingMaskIntoConstraints = false
+            line.backgroundColor = self.borderColor
+            self.addSubview(line)
+            
+            let views = ["line": line]
+            let metrics = ["lineWidth": newValue]
+            self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("[line(==lineWidth)]|", options: [], metrics: metrics, views: views))
+            self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[line]|", options: [], metrics: nil, views: views))
+        }
+    }
+    
+    @IBInspectable var bottomBorder: CGFloat {
+        get {
+            return 0.0
+        }
+        set {
+            let line = UIView(frame: CGRect(x: 0.0, y: self.bounds.height, width: self.bounds.width, height: newValue))
+            line.translatesAutoresizingMaskIntoConstraints = false
+            line.backgroundColor = self.borderColor
+            self.addSubview(line)
+            
+            let views = ["line": line]
+            let metrics = ["lineWidth": newValue]
+            self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|[line]|", options: [], metrics: nil, views: views))
+            self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[line(==lineWidth)]|", options: [], metrics: metrics, views: views))
+        }
+    }
+}
